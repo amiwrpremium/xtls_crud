876e35b560126085718a229d1887927f1e047a7c
amiwrpremium <amiwrpremium@gmail.com>
2022-11-28 00:30:24 +0330

    inbounds models

---
+++ xtls_crud/xtls_crud/models/inbounds/__init__.py
@@ -0,0 +1,9 @@
+from .sniffing import Sniffing
+
+from .settings import Client
+from .settings import Setting
+
+from .stream_settings import Certificate
+from .stream_settings import TlsSettings
+from .stream_settings import WsSettings
+from .stream_settings import StreamSettings
