079cd4dc15073d362cc259ac6980cc1e08ca7413
amiwrpremium <amiwrpremium@gmail.com>
2022-11-28 00:31:59 +0330

    Create builder.py

---
+++ xtls_crud/xtls_crud/utils/inbounds/builder.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+
+class Builder(ABC):
+    @abstractmethod
+    def build(self):
+        ...
+
+    def check(self):
+        for k, v in self.__dict__.items():
+            if v is None:
+                raise ValueError(f"{k} is None")
