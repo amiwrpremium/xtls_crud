4ef5b156507754f4aad8959c7ecfe97e8054684f
amiwrpremium <amiwrpremium@gmail.com>
2022-11-29 16:11:24 +0330

    inbound__post__js__sample

--- xtls_crud/web/backend/app/open_api/code_samples/js/__init__.py
+++ xtls_crud/web/backend/app/open_api/code_samples/js/__init__.py
@@ -21,6 +21,46 @@ axios.request(options).then(function (response) {{
     label="JavaScript"
 )
 
+inbound__post__js__sample = CodeSample(
+    lang="JavaScript",
+    source=f"""
+const axios = require('axios');
+
+const options = {{
+    method: 'POST',
+    url: '{settings.SERVER_HOST}/api/v1/inbounds/',
+    headers: {{
+        'Content-Type': 'application/json'
+    }},
+    data: {{
+        "user_id": 1,
+        "up": "100GB",
+        "down": 107374182400,
+        "total": 0,
+        "remark": "amiwrpremium",
+        "enable": true,
+        "expiry_time": "1MO",
+        "listen": "as",
+        "port": 443,
+        "protocol": "vmess",
+        "uuid": "f80365b4-ef07-4d39-a938-5275dc5db89e",
+        "network": "ws",
+        "security": "tls",
+        "server_name": "http://127.0.0.1/",
+        "ws_path": "/alTdjZ",
+        "tag": 1,
+        "sniffing": true
+    }}
+}};
+
+axios.request(options).then(function (response) {{
+    console.log(response.data);
+}}).catch(function (error) {{
+    console.error(error);
+}});
+""",
+    label="JavaScript"
+)
 
 builders__post__js__sample = CodeSample(
     lang="JavaScript",
