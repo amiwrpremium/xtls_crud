ad18f97d213aed6c38bbf2fc3190b628f02841e2
amiwrpremium <amiwrpremium@gmail.com>
2022-11-29 12:20:22 +0330

    code samples

---
+++ xtls_crud/web/backend/app/open_api/code_samples/samples.py
@@ -0,0 +1,21 @@
+from .python import inbound__get__python__sample, builders__post__python__sample
+from .js import inbound__get__js__sample, builders__post__js__sample
+from .models import XCodeSample
+
+inbound__get = XCodeSample(
+    path="/api/v1/inbounds/",
+    method="get",
+    samples=[
+        inbound__get__python__sample,
+        inbound__get__js__sample,
+    ]
+)
+
+builders__post = XCodeSample(
+    path="/api/v1/builders/",
+    method="post",
+    samples=[
+        builders__post__python__sample,
+        builders__post__js__sample
+    ]
+)
