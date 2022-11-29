9df0d7d8b657bd31e5a63a6df6ad6cf24713fcf1
amiwrpremium <amiwrpremium@gmail.com>
2022-11-29 16:11:35 +0330

    inbound__post__python__sample

--- xtls_crud/web/backend/app/open_api/code_samples/python/__init__.py
+++ xtls_crud/web/backend/app/open_api/code_samples/python/__init__.py
@@ -18,6 +18,44 @@ print(response.json())
     label="Python"
 )
 
+inbound__post__python__sample = CodeSample(
+    lang="Python",
+    source=f"""
+import requests
+
+url = "{settings.SERVER_HOST}/api/v1/inbounds/"
+
+payload = {{
+    "user_id": 1,
+    "up": "100GB",
+    "down": 107374182400,
+    "total": 0,
+    "remark": "amiwrpremium",
+    "enable": true,
+    "expiry_time": "1MO",
+    "listen": "as",
+    "port": 443,
+    "protocol": "vmess",
+    "uuid": "f80365b4-ef07-4d39-a938-5275dc5db89e",
+    "network": "ws",
+    "security": "tls",
+    "server_name": "http://127.0.0.1/",
+    "ws_path": "/alTdjZ",
+    "tag": 1,
+    "sniffing": true
+}}
+headers = {{
+    'Content-Type': 'application/json'
+}}
+
+response = requests.request("POST", url, headers=headers, json=payload)
+
+print(response.json())
+""",
+    label="Python"
+)
+
+
 builders__post__python__sample = CodeSample(
     lang="Python",
     source=f"""
