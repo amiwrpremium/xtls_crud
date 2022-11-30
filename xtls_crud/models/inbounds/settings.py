838aaa2db625df94c58243aefe9bee1ae1d92427
amiwrpremium <amiwrpremium@gmail.com>
2022-11-30 15:26:06 +0330

    Update settings.py

--- xtls_crud/models/inbounds/settings.py
+++ xtls_crud/models/inbounds/settings.py
@@ -6,21 +6,65 @@ from pydantic import BaseModel, UUID4, UUID1
 
 
 class Client(BaseModel):
+    """
+    Client
+
+    Keyword Args:
+        id (Optional[Union[UUID4, UUID1]]): id (default: uuid4())
+        alterId (Optional[int]): alterId (default: 0)
+
+    Returns:
+        Client (Client): Client
+    """
+
     id: Optional[Union[UUID4, UUID1]] = uuid4()
     alterId: Optional[int] = 0
 
 
 class PrettyClient(Client):
+    """
+    Pretty Client
+
+    Keyword Args:
+        id (str): id
+        alterId (int): alterId
+
+    Returns:
+        PrettyClient (PrettyClient): Pretty Client
+    """
+
     id: str
     alterId: int
 
 
 class Setting(BaseModel):
+    """
+    Setting
+
+    Keyword Args:
+        clients (List[Client]): clients
+        disableInsecureEncryption (Optional[bool]): disableInsecureEncryption (default: False)
+
+    Returns:
+        Setting (Setting): Setting
+    """
+
     clients: List[Client]
     disableInsecureEncryption: Optional[bool] = False
 
 
 class PrettySetting(Setting):
+    """
+    Pretty Setting
+
+    Keyword Args:
+        clients (List[PrettyClient]): clients
+        disableInsecureEncryption (bool): disableInsecureEncryption
+
+    Returns:
+        PrettySetting (PrettySetting): Pretty Setting
+    """
+
     clients: List[PrettyClient]
     disableInsecureEncryption: bool
 
