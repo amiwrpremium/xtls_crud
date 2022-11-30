3549b1f75319d68c737a1c951264a4f4996c2f6a
amiwrpremium <amiwrpremium@gmail.com>
2022-11-30 14:19:41 +0330

    Update __init__.py

---
+++ xtls_crud/commands/__init__.py
@@ -0,0 +1,17 @@
+"""
+# XTLS_CRUD CLI commands
+
+## Run web server
+```shell title="Run Webserver using default configurations"
+xtls_crud web serve
+```
+
+```shell title="Run Webserver using custom configurations"
+xtls_crud web serve --host="0.0.0.0" --port=8080
+```
+
+## See Webserver help
+```shell
+xtls_crud web serve --help
+```
+"""
